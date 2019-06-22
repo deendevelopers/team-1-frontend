917a3edf41c58dcc13880c49b456705a968ecaf7
zaarheed <zahid@zmdev.com>
2019-06-22 23:54:08 +0100

    animate MosqueListCard as user scrolls

--- src/components/MosqueListCard.tsx
+++ src/components/MosqueListCard.tsx
@@ -4,10 +4,46 @@ import { Link } from 'react-router-dom';
 import '../css/MosqueListCard.css';
 
 class MosqueListCard extends Component<any> {
+
+	state = {
+		isActive: false
+	};
+
+	componentDidMount() {
+		document.addEventListener("scroll", ()=>{
+			let el = document.getElementById(`mosque-card-${this.props.id}`);
+
+			if (!el) {
+				return;
+			}
+
+			const loc = el.getBoundingClientRect();
+
+			if (loc.top > -65 && loc.top < 100) {
+				this.setState({
+					isActive: true
+				});
+			}
+			else {
+				this.setState({
+					isActive: false
+				})
+			}
+		});
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('scroll', (event) => {});
+	}
+
+	handleScroll() {
+		
+	}
+
 	render() {
 		return (
-			<Link className="mosque-list-card-link" to={`/mosque/${this.props.id}`}>
-				<div className="mosque-list-card-wrapper">
+			<Link className="mosque-list-card-link" id={ `mosque-card-${this.props.id}` } to={`/mosque/${this.props.id}`}>
+				<div className={ "mosque-list-card-wrapper " + (this.state.isActive ? "active" : "") }>
 					<div className="mosque-list-card">
 						<div className="mosque-thumbnail-wrapper">
 							<img
