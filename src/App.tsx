76c63ffb4b02aee6a32b2029dc3c8b762ac5f5ac
zaarheed <zahid@zmdev.com>
2019-06-22 12:15:41 +0100

    initial router setup and initial login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { IonApp, IonPage, IonRouterOutlet } from '@ionic/react';
+import LoginScreen from './pages/LoginScreen';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router>
+        <div className="App">
+          <IonApp>
+            <IonPage id="main">
+              <IonRouterOutlet>
+                <Route exact path="/" component={LoginScreen} />
+              </IonRouterOutlet>
+            </IonPage>
+          </IonApp>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/LoginScreen.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+
+class LoginScreen extends Component {
+	render() {
+		return (
+			<>
+                <img src={logo} alt="logo"></img>
+				Big man ting
+			</>
+		);
+	}
+}
+
+export default LoginScreen;
